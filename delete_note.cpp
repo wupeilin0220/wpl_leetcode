5da99749fe05201d5af781e4c5855089f0956c95
peilin.wu <peilin0220@163.com>
2023-04-14 21:15:36 +0800

    方法二：使用栈

--- delete_note.cpp
+++ delete_note.cpp
@@ -14,8 +14,7 @@
  */
 class Solution {
 public:
-    // 遍历两次，第一次获得链表总长度，然后根据传参获取要删除节点的正序位置，然后删除；
-    // 感觉遍历两次挺费劲的；
+    // 方法一：遍历两次，第一次获得链表总长度，然后根据传参获取要删除节点的正序位置，然后删除；
     ListNode* removeNthFromEnd(ListNode* head, int n) {
         ListNode* curr = head;
         int list_size = 0;
@@ -41,5 +40,28 @@ public:
             }
         }
         return curr;
+    }
+
+    // 方法二：使用栈存储节点，然后根据n值pop()，再改变指针指向完成删除节点操作；
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* dummy = new ListNode(0, head);
+        ListNode* cur = dummy;
+        stack<ListNode*> store_node;
+        int pos = 1;
+        while(cur != nullptr){
+            store_node.push(cur);
+            cur = cur->next;
+        }
+
+        // 找出被删除节点的前一个节点；
+        for(int i =0 ; i <n; i++){
+            store_node.pop();
+        }
+        ListNode* pre = store_node.empty() ? nullptr : store_node.top();
+        if(pre == nullptr){
+            return nullptr;
+        }
+        pre->next = pre->next->next;
+        return dummy->next;
     }
 };
